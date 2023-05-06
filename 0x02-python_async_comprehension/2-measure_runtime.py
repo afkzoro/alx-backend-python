f27c84f39d38a272c1e2509d8b0720fcad8d60d8
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 11:53:33 +0100

    commit

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+"""The total runtime of 10 seconds is due to the fact that each call to
+async_comprehension is awaiting for 10 random numbers to be generated with
+a one-second delay between each number """
+import asyncio
+from time import perf_counter
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """_summary_
+
+ Returns:
+     float: _description_
+ """
+    start_time = perf_counter()
+    await asyncio.gather(*[async_comprehension() for i in range(4)])
+    end_time = perf_counter()
+    return end_time - start_time
