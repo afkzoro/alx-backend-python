b780eba3129d723a157409cb50aefbe7d76aa5c1
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 11:06:40 +0100

    commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async generator """
 import asyncio
 from typing import AsyncGenerator
+import random
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Asynchronous generator that yields a random float between 0 and 10
     """
