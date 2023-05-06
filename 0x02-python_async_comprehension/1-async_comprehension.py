ec5f9ad9f8509ad61c77ec991c29cf3086d30a25
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 11:33:21 +0100

    commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" write a coroutine called async_comprehension that takes no arguments """
+from typing import List
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[int]:
+    """_summary_
+
+ Returns:
+     List[int]: _description_
+ """
+
+    return [x async for x in async_generator()]
