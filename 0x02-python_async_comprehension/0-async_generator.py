db6970877c845cee6aa5084347958ec0170f2a72
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-06 11:11:33 +0100

    commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-""" Async generator """
+""" Async generator in python """
 import asyncio
 from typing import AsyncGenerator
 import random
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Asynchronous generator that yields a random float between 0 and 10
-    """
+    """Asynchronous generator that yields a random float between 0 and 10
+
+ Returns:
+     AsyncGenerator[float, None]: _description_
+
+ Yields:
+     Iterator[AsyncGenerator[float, None]]: _description_
+ """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
