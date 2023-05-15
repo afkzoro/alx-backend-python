83a59cfb1ad41adcecdbf1a4ce71606c08a77997
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-15 23:30:10 +0100

    fix return value

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,8 +4,11 @@ parameters and return values with the appropriate types"""
 from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable[Sequence[str]]) -> List[
- Tuple[Sequence[str], int]]:
+def element_length(lst: Iterable[Sequence]) -> List[
+ Tuple[Sequence, int]]:
     """ Well annotated function
     """
     return [(i, len(i)) for i in lst]
+
+
+print(element_length.__annotations__)
