7774beb68b43c3f0d25cc9640ad2dd3d701c9f64
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-23 13:17:00 +0100

    commit

--- 0x03-Unittests_and_integration_tests/testclass.py
+++ 0x03-Unittests_and_integration_tests/testclass.py
@@ -2,10 +2,14 @@ from utils import memoize
 
 
 class TestClass:
-
+    """ Test class"""
     def a_method(self):
+        """ a_method
+        """
         return 42
 
     @memoize
     def a_property(self):
+        """ a_property
+        """
         return self.a_method()
