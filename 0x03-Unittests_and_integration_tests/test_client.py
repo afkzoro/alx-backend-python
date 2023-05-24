7e8b6da326d81677eb07a32e73df1b4f7a9841fe
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-24 06:54:18 +0100

    update unit tests for client

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Unit tests for the GithubOrgClient class.
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Test class for the GithubOrgClient.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """
+        Test the org method of GithubOrgClient.
+
+        Args:
+            org_name (str): The organization name.
+            mock_get_json (Mock): The mocked get_json function.
+
+        """
+        test_payload = {"org": org_name}
+        mock_get_json.return_value = test_payload
+
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, test_payload)
+
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
